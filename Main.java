cb434e1923e5c9bf243a5ccbf1151329274070e4
kirill160 <81866092+kirill160@users.noreply.github.com>
2022-08-23 19:22:20 +0300

    test

---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.company.Function.Lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+       
+    }
+    static int proisv(){
+        int n = 10;
+        int prois = 1;
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int)(Math.random() * 20 + 1);
+            prois *=a[i];
+
+        }
+        return prois;
+    }
+    static int additionPositiveElements(){
+        int n = 8;
+        int summ = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] =(int) ((Math.random() * 10 + 10 + 1) - 10);
+            if(arr[i] > 0){
+                summ += arr[i];
+            }
+        }
+        return summ;
+    }
+    static int elements(){
+        int n = 16;
+        int summ = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 20 + 1);
+            if (arr[i] % 3 == 0 && arr[i] % 5 == 0){
+                summ++;
+            }
+
+        }
+        return summ;
+    }
+    static int evenValues(){
+        int n = 9;
+        int summ = 0;
+        int[] arr  = new int [n];
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] % 2 == 0){
+                summ +=arr[i];
+            }
+        }
+    return summ;
+    }
+}
